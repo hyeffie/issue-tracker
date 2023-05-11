c9c46323fa2b31118ba7b20a0fc1b9f6accc931c
JaydenLee1116 <vv55adss@gmail.com>
2023-05-11 23:52:56 +0900

    feat #4: filter-bar 컴포넌트 구현
    - only UI
    - 기능 추가 예정

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './Header/Header';
+import FilterBar from './FilterBar/FilterBar';
 
 function App() {
   return (
     <div className="App h-screen px-10 py-6">
       <Header url="https://images.unsplash.com/photo-1600354587397-681c16c184bf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80" />
+      <FilterBar />
     </div>
   );
 }
