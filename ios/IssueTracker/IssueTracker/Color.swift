1e77982fb0b844e8217f066768f541f815c6228b
Hyejeong Park <effie.thedev@gmail.com>
2023-05-11 20:59:50 +0900

    chore (#21): 컬러, 이미지 에셋 추가
    - color default value 추가 (any, dark 동일)
    - Color 열거형 추가
    - logotype 및 icon 리소스 svg 파일 추가

---
+++ ios/IssueTracker/IssueTracker/Color.swift
@@ -0,0 +1,28 @@
+//
+//  Color.swift
+//  IssueTracker
+//
+//  Created by Effie on 2023/05/11.
+//
+
+import UIKit
+
+enum Color: String {
+  case gray50
+  case gray100
+  case gray200
+  case gray300
+  case gray400
+  case gray500
+  case gray600
+  case gray700
+  case gray800
+  case gray900
+  case blue
+  case red
+  case purple
+  
+  var color: UIColor {
+    UIColor(named: self.rawValue) ?? .white
+  }
+}
