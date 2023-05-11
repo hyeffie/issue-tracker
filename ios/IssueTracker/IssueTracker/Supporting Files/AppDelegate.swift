12185a0b9d7df138cf14f8ca09abe19422788929
Hyejeong Park <effie.thedev@gmail.com>
2023-05-11 11:39:45 +0900

    chore (#7): SwiftLint 추가
    - SwiftLint 실행 스크립트 및 yml 파일 추가
    - 규칙에 맞도록 코드 정리
    - 프로젝트 그룹 정리

---
+++ ios/IssueTracker/IssueTracker/Supporting Files/AppDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  AppDelegate.swift
+//  IssueTracker
+//
+//  Created by Effie on 2023/05/11.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate { }
---
+++ ios/IssueTracker/IssueTracker/Supporting Files/SceneDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  SceneDelegate.swift
+//  IssueTracker
+//
+//  Created by Effie on 2023/05/11.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  var window: UIWindow?
+}
