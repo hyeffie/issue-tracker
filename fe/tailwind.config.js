8531ffb84fd9568caa15bb0435f23aa4fe417535
Hannah Ahn <ahnlook90@gmail.com>
2023-05-12 11:25:59 +0900

    design #4: 테일윈드 색상 속성 추가

--- fe/tailwind.config.js
+++ fe/tailwind.config.js
@@ -2,6 +2,21 @@
 
 export const content = ['./src/**/*.{js,jsx,ts,tsx}'];
 export const theme = {
-  extend: {},
+  extend: {
+    colors: {
+      neutral: '#4E4B66',
+      'neutral-week': '#6E7191',
+      'neutral-strong': '#14142B',
+      'light-mode': '#F7F7FC',
+    },
+  },
+  // TODO(Lily): set default attributes (font color)
+  // plugins: [
+  //   plugin(({ addBase, theme }) => {
+  //     addBase({
+  //       html: { color: theme('colors.slate.800') },
+  //     });
+  //   }),
+  // ],
 };
 export const plugins = [];
