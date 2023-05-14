4f951dd1bc63dab504cc4f2f8f07eae0a87176bd
JaydenLee1116 <vv55adss@gmail.com>
2023-05-14 15:43:35 +0900

    design #4: tsconfig theme 기본 컬러 추가
    - theme에 적용할 스타일 논의 필요

--- fe/tailwind.config.js
+++ fe/tailwind.config.js
@@ -4,6 +4,21 @@ export const content = ['./src/**/*.{js,jsx,ts,tsx}'];
 export const theme = {
   extend: {
     colors: {
+      // NOTE(Jayden): 기본 컬러 설정
+      'gray-50': '#FEFEFE',
+      'gray-100': '#F7F7FC',
+      'gray-200': '#EFF0F6',
+      'gray-300': '#D9DBE9',
+      'gray-400': '#BEC1D5',
+      'gray-500': '#A0A3BD',
+      'gray-600': '#6E7191',
+      'gray-700': '#4E4B66',
+      'gray-800': '#2A2A44',
+      'gray-900': '#14142B',
+      blue: '#007AFF',
+      navy: '#0025E6',
+      red: '#FF3B30',
+
       neutral: '#4E4B66',
       'neutral-weak': '#6E7191',
       'neutral-strong': '#14142B',
