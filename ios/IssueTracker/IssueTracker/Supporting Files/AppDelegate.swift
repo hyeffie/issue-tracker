dc5bdd2d388de70ca5185f9262431d81b6d6a4fd
Hyejeong Park <effie.thedev@gmail.com>
2023-05-14 19:41:23 +0900

    refactor: #32: scene connection 발생시 tab bar controller를 표시하는 로직 추가
    - windowScene delegate의 willConnectTo 메소드에 tab bar controller 표시 로직 추가
    - app delegate 의 didLaunchWithOptions 메소드에도 같은 로직 추가
    - 참고) 로그인 조건은 아직 미구현 상태

--- ios/IssueTracker/IssueTracker/Supporting Files/AppDelegate.swift
+++ ios/IssueTracker/IssueTracker/Supporting Files/AppDelegate.swift
@@ -8,4 +8,19 @@
 import UIKit
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate { }
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+  
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil)
+  -> Bool
+  {
+    window = UIWindow(frame: UIScreen.main.bounds)
+    
+    let tabBarController = TabBarController()
+    window?.rootViewController = tabBarController
+    window?.makeKeyAndVisible()
+    return true
+  }
+}
--- ios/IssueTracker/IssueTracker/Supporting Files/SceneDelegate.swift
+++ ios/IssueTracker/IssueTracker/Supporting Files/SceneDelegate.swift
@@ -9,4 +9,17 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   var window: UIWindow?
+  
+  func scene(
+    _ scene: UIScene,
+    willConnectTo session: UISceneSession,
+    options connectionOptions: UIScene.ConnectionOptions)
+  {
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+    window = UIWindow(windowScene: windowScene)
+    
+    let tabBarController = TabBarController()
+    window?.rootViewController = tabBarController
+    window?.makeKeyAndVisible()
+  }
 }
