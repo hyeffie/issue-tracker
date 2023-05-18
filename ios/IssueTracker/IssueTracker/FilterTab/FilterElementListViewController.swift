b25143b55d57fb9989bdf5f6a2bbfbb9eaed6236
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2023-05-18 15:37:20 +0900

    feat #48: 필터 요소 목록 화면 추가
    - FilterListViewController 클래스 선언 및 스토리보드 연결
    - IssueList에서 Modal을 이용하여 화면 이동
    - Navigation Bar Item 추가
    - collectionView 추가

---
+++ ios/IssueTracker/IssueTracker/FilterTab/FilterElementListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FilterListViewController.swift
+//  IssueTracker
+//
+//  Created by 1 on 2023/05/18.
+//
+
+import UIKit
+
+class FilterElementListViewController: UIViewController {
+   @IBOutlet weak var collectionView: UICollectionView!
+   
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+   }
+}
