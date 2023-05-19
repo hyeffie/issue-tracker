d7d67b152d07bf98d3dff4a964b09f54ec518ee6
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2023-05-19 12:02:46 +0900

    feat #48: 필터 목록 Layout 설정
    - Section에 Inset을 부여하여 Section - Item 사이 구분할 수 있도록 UI 구현
    - Cell 사이 LineSpacing 부여
    - Cell 크기 지정
    - 불필요한 IssueListUseCase 삭제

--- ios/IssueTracker/IssueTracker/IssueTab/IssueListFilter/FilterListViewController.swift
+++ ios/IssueTracker/IssueTracker/IssueTab/IssueListFilter/FilterListViewController.swift
@@ -107,3 +107,31 @@ extension FilterListViewController: UICollectionViewDataSource {
       return cell
    }
 }
+
+extension FilterListViewController: UICollectionViewDelegateFlowLayout {
+   func collectionView(_ collectionView: UICollectionView,
+                       layout collectionViewLayout: UICollectionViewLayout,
+                       sizeForItemAt indexPath: IndexPath) -> CGSize {
+      return CGSize(width: collectionView.frame.width, height: 44)
+   }
+   
+   func collectionView(_ collectionView: UICollectionView,
+                       layout collectionViewLayout: UICollectionViewLayout,
+                       referenceSizeForHeaderInSection section: Int) -> CGSize {
+      return CGSize(width: collectionView.frame.width, height: 44)
+   }
+   
+   func collectionView(
+      _ collectionView: UICollectionView,
+      layout collectionViewLayout: UICollectionViewLayout,
+      minimumLineSpacingForSectionAt section: Int
+   ) -> CGFloat {
+      return 1.0
+   }
+
+   func collectionView(_ collectionView: UICollectionView,
+                       layout collectionViewLayout: UICollectionViewLayout,
+                       insetForSectionAt section: Int) -> UIEdgeInsets {
+      return UIEdgeInsets(top: 1.0, left: 0, bottom: 4.0, right: 0)
+   }
+}
