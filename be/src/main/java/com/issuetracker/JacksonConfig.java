3bb2fd5b1e1f81d1aba0ccaa0daa1fe9c660c2c5
acceptor-gyu <gseonggyu968@gmail.com>
2023-05-23 15:37:51 +0900

    fix: request로 들어오는 시간 타입 objectMapper로 변경되도록 함

--- be/src/main/java/com/issuetracker/JacksonConfig.java
+++ be/src/main/java/com/issuetracker/JacksonConfig.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -27,6 +30,12 @@ public class JacksonConfig {
             }
         });
 
+        module.addDeserializer(LocalDateTime.class, new JsonDeserializer<LocalDateTime>() {
+            @Override
+            public LocalDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+                return LocalDateTime.parse(jsonParser.getValueAsString(), DateTimeFormatter.ofPattern("yyyy-MM-dd kk:mm:ss"));
+            }
+        });
 
         return module;
     }
