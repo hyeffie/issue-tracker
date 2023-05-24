42cbd75bcc4c846fb432d41e33b6a8107f8392d5
acceptor-gyu <gseonggyu968@gmail.com>
2023-05-25 07:55:56 +0900

    chore: TODO 주석 추가

--- be/src/main/java/com/issuetracker/controller/CommentController.java
+++ be/src/main/java/com/issuetracker/controller/CommentController.java
@@ -17,6 +17,8 @@ public class CommentController {
     // TODO: issueId는 query parameter로, writer와 content는 request body로 들어오는데 어떻게 넘겨줄까
     @PostMapping("/api/issues/{issueId}")
     public void post(@RequestBody CommentPostDto commentPostDto, @PathVariable long issueId) {
+
+        // TODO: 예외처리 (없거나 삭제된 issue라면 400 또는 404 에러)
         commentService.createComment(commentPostDto, issueId);
     }
 }
