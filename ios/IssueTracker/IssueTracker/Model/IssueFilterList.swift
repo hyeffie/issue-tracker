230769e4d506cadf97c17b346466ad1657209bf7
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2023-05-28 12:33:56 +0900

    feat #111: 필터목록에 사용될 모델 생성
    - 초기화시 default value 적용

---
+++ ios/IssueTracker/IssueTracker/Model/IssueFilterList.swift
@@ -0,0 +1,25 @@
+//
+//  IssueFilterList.swift
+//  IssueTracker
+//
+//  Created by Wood on 2023/05/28.
+//
+
+import Foundation
+
+struct IssueFilterList {
+   var statusList: [String]
+   var userList: [IssueListDTO.User]
+   var labelList: [IssueListDTO.Label]
+   var milestoneList: [IssueListDTO.Milestone]
+   
+   init(statusList: [String] = ["열린 이슈", "내가 작성한 이슈", "내가 댓글을 남긴 이슈", "닫힌 이슈"],
+        userList: [IssueListDTO.User] = [],
+        labelList: [IssueListDTO.Label] = [],
+        milestoneList: [IssueListDTO.Milestone] = []) {
+      self.statusList = statusList
+      self.userList = userList
+      self.labelList = labelList
+      self.milestoneList = milestoneList
+   }
+}
