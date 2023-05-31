8cbbeff667b0d76ed633f95773ae91c395b3343c
Hyejeong Park <effie.thedev@gmail.com>
2023-05-31 21:39:57 +0900

    feat #147: navigation item 추가
    - larget title mode 설정
    - segmented control 추가
    - 저장 버튼 추가
    - stack view 상단 view 추가

--- ios/IssueTracker/IssueTracker/IssueTab/Create/IssueCreateViewController.swift
+++ ios/IssueTracker/IssueTracker/IssueTab/Create/IssueCreateViewController.swift
@@ -10,5 +10,18 @@ import UIKit
 class IssueCreateViewController: UIViewController, StoryboardBased {
    override func viewDidLoad() {
       super.viewDidLoad()
+      
+      setNavigationItem()
+   }
+   
+   private func setNavigationItem() {
+      self.navigationItem.largeTitleDisplayMode = .never
+      let segmentedControl = UISegmentedControl(items: ["마크다운", "미리보기"])
+      segmentedControl.selectedSegmentIndex = 0
+      self.navigationItem.titleView = segmentedControl
+      
+      let saveButton = UIBarButtonItem(title: "저장", style: .plain, target: self, action: nil)
+      saveButton.isEnabled = false
+      self.navigationItem.rightBarButtonItem = saveButton
    }
 }
