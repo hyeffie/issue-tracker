e294247eb81a117b86521cf99481617354063eb7
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2023-05-31 10:59:35 +0900

    feat #130: 이슈 닫기에 필요한 DTO 생성
    - 이전 파일이동 반영부분 ViewController에서 삭제

--- ios/IssueTracker/IssueTracker/IssueTab/List/IssueListViewController.swift
+++ ios/IssueTracker/IssueTracker/IssueTab/List/IssueListViewController.swift
@@ -20,13 +20,13 @@ class IssueListViewController: UIViewController, UIToolbarDelegate {
    private var filterList = IssueFilterList()
    private var filterApplyList: FilterApplyList? = nil
    private var searchQuery: String? = nil
-   
-   private var selectToolbar: SelectToolBar?
-   
+
+   var selectToolbar: SelectToolBar?
    var currentPageNumber: Int = 1
    var isPaging = false
    var hasNextPage = true
    var isSelectMode = false
+   let toolbarTag: Int = 100
    
    override func viewDidLoad() {
       super.viewDidLoad()
@@ -320,78 +320,5 @@ extension IssueListViewController: UISearchBarDelegate {
       self.searchQuery = nil
       self.reset()
       self.fetchIssues()
-   }
-}
-
-// MARK: - Select Mode
-
-extension IssueListViewController {
-   @objc func toggleSelectMode() {
-      isSelectMode = true
-      self.navigationItem.title = "이슈 선택"
-      self.navigationItem.leftBarButtonItem?.isHidden = true
-      self.navigationItem.rightBarButtonItem = createCancelButton()
-      self.collectionView.allowsMultipleSelection = true
-      configureTabBar(isHiding: true)
-      self.selectToolbar = createSelectToolBar()
-   }
-   
-   func createSelectToolBar() -> SelectToolBar? {
-      let nib = UINib(nibName: "SelectToolBar", bundle: nil)
-      guard let toolBar = nib.instantiate(withOwner: self, options: nil).first as? SelectToolBar else {
-         return nil
-      }
-      
-      toolBar.delegate = self
-      self.view.addSubview(toolBar)
-      toolBar.translatesAutoresizingMaskIntoConstraints = false
-      NSLayoutConstraint.activate([
-         toolBar.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
-         toolBar.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
-         toolBar.topAnchor.constraint(equalTo: (self.tabBarController?.tabBar.topAnchor)!),
-         toolBar.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
-      ])
-      
-      return toolBar
-   }
-   
-   func createCancelButton() -> UIBarButtonItem {
-      return UIBarButtonItem(title: "취소",
-                             style: .plain,
-                             target: self,
-                             action: #selector(cancelSelectMode))
-   }
-   
-   @objc func cancelSelectMode() {
-      self.navigationItem.leftBarButtonItem?.isHidden = false
-      setSelectButton()
-      configureTabBar(isHiding: false)
-      deselectAllCells()
-      self.collectionView.allowsMultipleSelection = false
-      self.isSelectMode = false
-      self.navigationItem.title = "이슈"
-   }
-   
-   private func setSelectButton() {
-      let selectButton = UIBarButtonItem(
-         title: "선택",
-         style: .plain,
-         target: self,
-         action: #selector(toggleSelectMode))
-      self.navigationItem.rightBarButtonItems = [selectButton]
-   }
-   
-   func deselectAllCells() {
-      guard let indexPaths = collectionView.indexPathsForSelectedItems else {
-         return
-      }
-      
-      indexPaths.forEach {
-         guard let cell = collectionView.cellForItem(at: $0) as? IssueListCollectionViewCell else {
-            return
-         }
-         
-         cell.didDeSelect()
-      }
    }
 }
---
+++ ios/IssueTracker/IssueTracker/Model/IssuePatchDTO.swift
@@ -0,0 +1,34 @@
+//
+//  IssuePatchDTO.swift
+//  IssueTracker
+//
+//  Created by Wood on 2023/05/31.
+//
+
+import Foundation
+
+class IssuePatchDTO: Encodable {
+   struct Issue: Encodable {
+      var issueId: Int
+      var opened: Bool
+      
+      init(issueId: Int, opened: Bool = false) {
+         self.issueId = issueId
+         self.opened = opened
+      }
+   }
+   
+   var issues: [Issue]
+   
+   init(issues: [Issue] = []) {
+      self.issues = issues
+   }
+   
+   func add(issue: Issue) {
+      self.issues.append(issue)
+   }
+   
+   func remove(id: Int) {
+      self.issues.removeAll(where: { $0.issueId == id })
+   }
+}
